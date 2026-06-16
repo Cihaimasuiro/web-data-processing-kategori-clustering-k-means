aaba34134cba26882b384019625e0f3fae071de9
ekayuliantii <112328766+Cihaimasuiro@users.noreply.github.com>
2026-06-16 11:21:09 +0700

    feat: configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Aplikasi-web-analisis-penjualan-dengan-K-Means/',
   plugins: [react()],
 })
